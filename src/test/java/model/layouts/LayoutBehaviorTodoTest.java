5bf996fd3118accdfd7200c6e04bd1fed599b1e8
Evan Mohan <prooflion@gmail.com>
2026-04-24 23:49:49 -0600

    Add layout behavior TODO test scaffold for remaining downtown traffic polish tasks

---
+++ src/test/java/model/layouts/LayoutBehaviorTodoTest.java
@@ -0,0 +1,19 @@
+package model.layouts;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled("Starter test scaffolding for final polish and teammate handoff.")
+class LayoutBehaviorTodoTest {
+    @Test
+    void todo_verify_each_layout_seeded_agents_match_demo_expectations() {
+    }
+
+    @Test
+    void todo_compare_bus_and_bike_movement_differences_over_multiple_ticks() {
+    }
+
+    @Test
+    void todo_cover_persistence_edge_cases_for_comments_blank_lines_and_legacy_agents() {
+    }
+}
